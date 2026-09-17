9c8cda1bd7815d72ba0b8064b671184dd514d9cf
Fernando Basso <me@fernandobasso.dev>
2026-09-17 16:24:26 -0300

    feat: Improve version test case

--- spec/adocwiki_spec.rb
+++ spec/adocwiki_spec.rb
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 RSpec.describe Adocwiki do
-  it "has a version number" do
+  it "has a a proper version number" do
     expect(Adocwiki::VERSION).to match(/\d+\.\d+\.\d+/)
   end
 end
