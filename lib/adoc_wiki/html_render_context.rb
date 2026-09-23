37659de4d9fc414857993f7e1bb3bcaed8f74563
Fernando Basso <fbassodev@gmail.com>
2026-09-23 19:51:18 -0300

    chore: Add missing SPDX license to files

--- lib/adoc_wiki/html_render_context.rb
+++ lib/adoc_wiki/html_render_context.rb
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 module AdocWiki
   class HtmlRenderContext
     def initialize(
--- spec/adoc_wiki/builder_spec.rb
+++ spec/adoc_wiki/builder_spec.rb
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 require "spec_helper"
 
 RSpec.describe AdocWiki::Builder do
