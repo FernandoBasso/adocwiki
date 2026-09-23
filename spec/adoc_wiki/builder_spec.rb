2e496db6068b9bfe440087d58d0165f3d3998088
Fernando Basso <fbassodev@gmail.com>
2026-09-23 08:30:02 -0300

    feat: Wrap builder#walk() tests into its own describe block

--- spec/adoc_wiki/builder_spec.rb
+++ spec/adoc_wiki/builder_spec.rb
@@ -1,70 +1,72 @@
 require "spec_helper"
 
 RSpec.describe AdocWiki::Builder do
-  it 'walks a one-level sidebar' do
-    side_nav = <<~EOF
-      Command Line:
-        - cmdline/bash.adoc
-        - cmdline/sed.adoc
-    EOF
+  describe '#walk()' do
+    it 'walks a one-level sidebar' do
+      side_nav = <<~EOF
+        Command Line:
+          - cmdline/bash.adoc
+          - cmdline/sed.adoc
+      EOF
 
-    builder = AdocWiki::Builder.new(side_nav:)
+      builder = AdocWiki::Builder.new(side_nav:)
 
-    expect(builder).to receive(:walk).with(no_args).once.and_call_original
+      expect(builder).to receive(:walk).with(no_args).once.and_call_original
 
-    expect(builder).to receive(:walk)
-      .with(["cmdline/bash.adoc", "cmdline/sed.adoc"])
-      .once
-      .and_call_original
-    
-    expect(builder).to receive(:conv)
-      .with('cmdline/bash.adoc')
-      .once
-      .and_call_original
+      expect(builder).to receive(:walk)
+        .with(["cmdline/bash.adoc", "cmdline/sed.adoc"])
+        .once
+        .and_call_original
 
-    expect(builder).to receive(:conv)
-      .with('cmdline/sed.adoc')
-      .once
-      .and_call_original
+      expect(builder).to receive(:conv)
+        .with('cmdline/bash.adoc')
+        .once
+        .and_call_original
 
-    builder.walk
-  end
+      expect(builder).to receive(:conv)
+        .with('cmdline/sed.adoc')
+        .once
+        .and_call_original
 
-  it 'walks a two-level sidebar' do
-    side_nav = <<~EOF
-      Command Line:
-        - cmdline/bash
-        - SED:
-          - cmdline/sed/intro.adoc
-    EOF
+      builder.walk
+    end
 
-    builder = AdocWiki::Builder.new(side_nav:)
+    it 'walks a two-level sidebar' do
+      side_nav = <<~EOF
+        Command Line:
+          - cmdline/bash
+          - SED:
+            - cmdline/sed/intro.adoc
+      EOF
 
-    expect(builder).to receive(:walk).with(no_args).once.and_call_original
+      builder = AdocWiki::Builder.new(side_nav:)
 
-    expect(builder).to receive(:walk)
-      .with(['cmdline/bash', {'SED' => ['cmdline/sed/intro.adoc']}])
-      .once
-      .and_call_original
+      expect(builder).to receive(:walk).with(no_args).once.and_call_original
 
-    expect(builder).to receive(:conv)
-      .with('cmdline/bash')
-      .once
+      expect(builder).to receive(:walk)
+        .with(['cmdline/bash', {'SED' => ['cmdline/sed/intro.adoc']}])
+        .once
+        .and_call_original
 
-    expect(builder).to receive(:walk)
-      .with({'SED' => ['cmdline/sed/intro.adoc']})
-      .once
-      .and_call_original
+      expect(builder).to receive(:conv)
+        .with('cmdline/bash')
+        .once
 
-    expect(builder).to receive(:walk)
-      .with(['cmdline/sed/intro.adoc'])
-      .once
-      .and_call_original
+      expect(builder).to receive(:walk)
+        .with({'SED' => ['cmdline/sed/intro.adoc']})
+        .once
+        .and_call_original
 
-    expect(builder).to receive(:conv)
-      .with('cmdline/sed/intro.adoc')
-      .once
-    
-    builder.walk()
+      expect(builder).to receive(:walk)
+        .with(['cmdline/sed/intro.adoc'])
+        .once
+        .and_call_original
+
+      expect(builder).to receive(:conv)
+        .with('cmdline/sed/intro.adoc')
+        .once
+
+      builder.walk()
+    end
   end
 end
