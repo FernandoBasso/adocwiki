54c2d7aa9ec48a1676912961477293437c140c8f
Fernando Basso <fbassodev@gmail.com>
2026-09-24 06:37:57 -0300

    doc: Document get_binding

--- lib/adoc_wiki/html_render_context.rb
+++ lib/adoc_wiki/html_render_context.rb
@@ -15,6 +15,26 @@ module AdocWiki
       @side_nav = side_nav
     end
 
+    ##
+    # The the binding context so we have the ivars available
+    # on the caller side. Specially useful for making them
+    # available in .erb templates.
+    #
+    # Example usage:
+    #
+    # ```ruby
+    # context = AdocWiki::HtmlRenderContext.new(
+    #   config: { site_title: "Site" },
+    #   adoc: nil,
+    #   side_nav: nil
+    # )
+    # template = "<h1><%= @config[:site_title] %></h1>"
+    # renderer = ERB.new(template)
+    # output = renderer.result(context.get_binding)
+    #
+    # expect(output).to eq("<h1>Site</h1>")
+    # ```
+    #
     def get_binding
       binding
     end
